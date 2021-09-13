ebf8a63a6773bf7761a45fd904a40f66d61658c2
Vitaly Shvedko <davids013013@gmail.com>
2021-09-13 19:43:03 +0300

    Add fixes for correct work Gradle with cyrillic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,17 @@
 import task1_fact_getter.FactGetter;
 import task2_nasa_photo_getter.NasaPhotoGetter;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static void main(String[] args) {
+//        Переопределение кодировки консольного вывода (в Maven не требуется)
+        System.setOut(new PrintStream(
+                new FileOutputStream(FileDescriptor.out), true, StandardCharsets.UTF_8));
+
         FactGetter.main();
         NasaPhotoGetter.main();
     }
